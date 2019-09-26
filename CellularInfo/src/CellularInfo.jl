30a6de955a1076ead1cbf17eb5059b169fa4ef14
Douglas G. Moore <doug@dglmoore.com>
2019-09-25 17:00:30 -0700

    Compute equivalences of ECA rules

--- CellularInfo/src/CellularInfo.jl
+++ CellularInfo/src/CellularInfo.jl
@@ -1,27 +1,11 @@
 module CellularInfo
 
-export simulate, tovideo
-
-include("te.jl")
-
 using Images, VideoIO
 
-function update!(state, rule)
-    if length(state) < 2
-        throw(DomainError(length(state), "update! requires that length(state) ≥ 2"))
-    end
-    left = state[end]
-    right = state[1]
-    temp = 2 * left + state[1]
-    for i in 2:length(state)
-        temp = 7 & (2 * temp + state[i])
-        state[i - 1] = 1 & (rule >> temp)
-    end
-    temp = 7 & (2 * temp + right)
-    state[end] = 1 & (rule >> temp)
+export simulate, tovideo, transferentropy
 
-    state
-end
+include("ca.jl")
+include("te.jl")
 
 function toimage(state, cellsize)
     img = fill(Gray(0.5), cellsize, length(state)*(cellsize + 1) - 1)
@@ -31,22 +15,12 @@ function toimage(state, cellsize)
     convert(Array{Gray{N0f8}}, img)
 end
 
-function simulate(rule, state::AbstractVector{Int}, t::Int)
-    traj = [state]
-    for _ in 1:t
-        push!(traj, update!(traj[end][:], rule))
+function tovideo(filename, series; cellwidth=10, framerate=25)
+    frames = []
+    for i in 1:size(series,1)
+        push!(frames, toimage(series[i,:], cellwidth))
     end
-    traj
-end
-
-function simulate(rule, width::Int, t::Int)
-    state = zeros(Int, width)
-    state[length(state) ÷ 2 + 1] = 1
-    simulate(rule, state, t)
-end
-
-function tovideo(filename, series; cellwidth=10)
-    encodevideo(filename, map(f -> toimage(f, cellwidth), series), framerate=25)
+    encodevideo(filename, frames, framerate=framerate)
 end
 
 end
---
+++ CellularInfo/src/ca.jl
@@ -0,0 +1,42 @@
+yflip(a::Int) = let D = [0,4,2,6,1,5,3,7]
+    reduce(|, ((1 & (a >> (i - 1))) << D[i] for i in 1:8))
+end
+
+complement(a::Int) = reduce(|, ((1 ⊻ (1 & (a >> ((i-1) ⊻ 7)))) << (i - 1) for i in 1:8))
+
+canonical(a::Int) = min(a, yflip(a), complement(a), complement(yflip(a)))
+
+const EQUIVALENT_RULES = Dict(c => (yflip(c), complement(c), complement(yflip(c))) for c in 0:255)
+const ECA_RULES = [Set(vcat(map(canonical, 0:255)))...]
+
+function update!(state, rule)
+    if length(state) < 2
+        throw(DomainError(length(state), "update! requires that length(state) ≥ 2"))
+    end
+    left = state[end]
+    right = state[1]
+    temp = 2 * left + state[1]
+    for i in 2:length(state)
+        temp = 7 & (2 * temp + state[i])
+        state[i - 1] = 1 & (rule >> temp)
+    end
+    temp = 7 & (2 * temp + right)
+    state[end] = 1 & (rule >> temp)
+
+    state
+end
+
+function simulate(rule, state::AbstractVector{Int}, t::Int)
+    traj = Array{Int}(undef, t + 1, length(state))
+    traj[1,:] = state
+    for i in 1:t
+        traj[i + 1, :] = update!(traj[i,:], rule)
+    end
+    traj
+end
+
+function simulate(rule, width::Int, t::Int)
+    state = zeros(Int, width)
+    state[length(state) ÷ 2 + 1] = 1
+    simulate(rule, state, t)
+end
