ba76a31dbd05da2d72f749da8c1f645d9b268e26
Douglas G. Moore <doug@dglmoore.com>
2019-09-27 10:58:50 -0700

    Compute and plot Mean TE

---
+++ CellularInfo/averages.jl
@@ -0,0 +1,58 @@
+using Distributed, JSON
+
+@everywhere begin
+    using Pkg
+    Pkg.activate(".")
+end
+
+@everywhere using CellularInfo
+
+@everywhere function transfer(traj, src, dst, k)
+    back = filter(f -> f != src && f != dst, 1:size(traj,2))
+    cond = traj[:,back]
+    transferentropy(traj[:,src], traj[:,dst], cond', k, 1000)
+    #  transferentropy(traj[:,src], traj[:,dst], k, 1000)
+end
+
+function main()
+    meante = zeros(Float64, 256)
+    for rule in 0:255
+        @time begin
+            @info "Building graph for $(rule)..."
+            k = 5
+            width = 101
+            duration = 200 + (width ÷ 2)
+
+            a = zeros(Int, width)
+            b = ones(Int, width)
+            a[width ÷ 2 + 1] = 1
+            b[width ÷ 2 + 1] = 0
+
+            traja = simulate(rule, a, duration)[width ÷ 2 + 1:end,:]
+            trajb = simulate(rule, b, duration)[width ÷ 2 + 1:end,:]
+
+            N = 2*width*(width - 1)
+            μ = 0.0
+            for src in 1:width
+                μ += @distributed (+) for dst in 1:width
+                    if src == dst
+                        0.0
+                    else
+                        tea = transfer(traja, src, dst, k)
+                        teb = transfer(trajb, src, dst, k)
+
+                        te = (tea.p < 0.05) ? tea.value : 0.0
+                        te += (teb.p < 0.05) ? teb.value : 0.0
+                    end
+                end
+            end
+            meante[rule + 1] = μ / N
+        end
+    end
+
+    open("meancondte.json", "w") do handle
+        JSON.print(handle, meante)
+    end
+end
+
+main()
---
+++ CellularInfo/plots.jl
@@ -0,0 +1,24 @@
+using CellularInfo, JSON, Plots, Plots.PlotMeasures
+
+μ = open(JSON.parse, "meante.json")
+class1 = μ[ECA_CLASS_I .+ 1]
+class2 = μ[ECA_CLASS_II .+ 1]
+class3 = μ[ECA_CLASS_III .+ 1]
+class4 = μ[ECA_CLASS_IV .+ 1]
+
+c12 = max(maximum(class1), maximum(class2))
+
+@show maximum(class1)
+@show maximum(class2)
+@show maximum(class3)
+@show maximum(class4)
+
+p = scatter(ECA_CLASS_I, class1, markershape=:cross, markercolor=:black, label="Class I", left_margin=1cm, bottom_margin=8mm)
+scatter!(p, ECA_CLASS_II, class2, markershape=:xcross, markercolor=:black, label="Class II")
+scatter!(p, ECA_CLASS_III, class3, markershape=:rect, markercolor=:green, label="Class III")
+scatter!(p, ECA_CLASS_IV, class4, markershape=:rect, markercolor=:orange, label="Class IV")
+hline!(p, [c12], linestyle=:dash, label="Class I & II Limit")
+xlabel!(p, "Rule")
+ylabel!(p, "Mean TE")
+title!(p, "Mean TE vs Rule by Wolfram Class")
+savefig(p, "meante.png")
