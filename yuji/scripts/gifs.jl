227c46eb5d8cb768bf19c24299f39ae7af484fb4
Douglas G. Moore <doug@dglmoore.com>
2019-12-23 12:07:33 -0700

    :hammer: Generate gifs for TE analysis

--- yuji/scripts/gifs.jl
+++ yuji/scripts/gifs.jl
@@ -46,51 +46,97 @@ function coarsegif(df)
 end
 
 function migif(df)
-    @unpack drug, phase, trial, gw, gh, mi, nperms = df
+    simdata = first(unique(select(df, :drug, :phase, :trial, :gw, :gh, :nperms)))
+    @unpack drug, phase, trial, gw, gh, nperms = simdata
 
     grid = @sprintf "%dx%d" gh gw
     basepath = projectdir("plots", drug, string(trial), grid, string(nperms))
     mkpath(basepath)
-    mkpath(joinpath(basepath, "frames"))
+    mkpath(joinpath(basepath, "frames", "mi"))
+
+    mis = sort(select(df, :lag, :mi), :lag)
 
     miframes = []
-    anim = @animate for i in eachindex(mi)
-        title = if i == 1
-            "Mutual Information (phase = $phase)"
-        elseif i == 2
-            "Mutual Information (phase = $phase), lag = $(i-1) frame)"
+    anim = @animate for row in eachrow(mis)
+        @unpack lag, mi = row
+
+        title = if lag == 0
+            @sprintf "Mutual Info %s %s" titlecase(phase) drug
+        elseif lag == 1
+            @sprintf "Mutual Info %s %s; Lagged %d Frame" titlecase(phase) drug lag
         else
-            "Mutual Information (phase = $phase), lag = $(i-1) frames)"
+            @sprintf "Mutual Info %s %s; Lagged %d Frames" titlecase(phase) drug lag
         end
 
-        p = heatmap(linearize(mi[i]),
+        p = heatmap(linearize(mi),
                     title=title,
                     titleloc=:left,
-                    xlabel="source cell",
-                    ylabel="target cell",
+                    xlabel="target cell",
+                    ylabel="source cell",
                     clim=(0,1))
         push!(miframes, p)
         p
     end
 
     for (i, p) in enumerate(miframes)
-        framename = @sprintf "%02d-%s.png" (i-1) phase
-        framefile = joinpath(basepath, "frames", framename)
+        framename = @sprintf "%d-%s.png" (i-1) phase
+        framefile = joinpath(basepath, "frames", "mi", framename)
         savefig(p, framefile)
     end
 
-    giffile = joinpath(basepath, df[:phase] * "-mi.gif")
+    giffile = joinpath(basepath, phase * "-mi.gif")
     gif(anim, giffile, fps=0.5)
 end
 
-function main(dirname="info")
-    df = collect_results!(datadir(dirname); subfolders=true)
-    for row in eachrow(df)
-        migif(row)
+function tegif(df)
+    simdata = first(unique(select(df, :drug, :phase, :trial, :gw, :gh, :nperms)))
+    @unpack drug, phase, trial, gw, gh, nperms = simdata
+
+    grid = @sprintf "%dx%d" gh gw
+    basepath = projectdir("plots", drug, string(trial), grid, string(nperms))
+    mkpath(basepath)
+    mkpath(joinpath(basepath, "frames", "te"))
+
+    tes = sort(select(df, :k, :te), :k)
+
+    teframes = []
+    anim = @animate for row in eachrow(tes)
+        @unpack k, te = row
+
+        title = @sprintf "Transfer Entropy %s %s; k=%d" titlecase(phase) drug k
+
+        p = heatmap(linearize(te),
+                    title=title,
+                    titleloc=:left,
+                    xlabel="target cell",
+                    ylabel="source cell",
+                    clim=(0,1))
+        push!(teframes, p)
+        p
     end
-    for row in eachrow(df[:,[:drug,:trial,:phase,:gh,:gw,:videopath]])
-        coarsegif(row)
-        binnedgif(row)
+
+    for (i, p) in enumerate(teframes)
+        framename = @sprintf "%d-%s.png" i phase
+        framefile = joinpath(basepath, "frames", "te", framename)
+        savefig(p, framefile)
+    end
+
+    giffile = joinpath(basepath, phase * "-te.gif")
+    gif(anim, giffile, fps=0.5)
+end
+
+function main()
+    df = collect_results(datadir("info", "mi"))
+    for group in groupby(df, [:drug, :trial, :phase, :gh, :gw])
+        migif(group)
+    end
+    df = collect_results(datadir("info", "te"))
+    for group in groupby(df, [:drug, :trial, :phase, :gh, :gw])
+        tegif(group)
+    end
+    for group in eachrow(unique(select(df, :drug, :trial, :phase, :gh, :gw, :videopath)))
+        coarsegif(group)
+        binnedgif(group)
     end
 end
 
